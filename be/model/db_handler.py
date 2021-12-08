2c175ce0ca7424e3dc7938c5357a208e7f87278b
yanghong <10195501424@stu.ecnu.edu.cn>
2021-12-08 16:35:47 +0800

    1

--- be/model/db_handler.py
+++ be/model/db_handler.py
@@ -7,7 +7,92 @@ from configparser import ConfigParser
 class DB_handler:
     def __init__(self):
         self.section = "postgresql"
-        self.config_path = "./database.ini"
+        self.config_path = "database.ini"
+        self.init_tables()
+
+    def init_tables(self):
+        commands = (
+            "CREATE TABLE USERS ( \
+            UID VARCHAR(255) PRIMARY KEY, \
+            PWD TEXT NOT NULL, \
+            SHOP_ID VARCHAR(255) NOT NULL, \
+            BALANCE FLOAT NOT NULL\
+            )\
+            ",
+            " CREATE TABLE SHOPS ( \
+            SHOP_ID VARCHAR(255) PRIMARY KEY, \
+            UID VARCHAR(255) NOT NULL, \
+            RANKING INTEGER\
+            )\
+            ",
+            " CREATE TABLE BOOKS ( \
+            BOOK_ID VARCHAR(255) PRIMARY KEY, \
+            UID VARCHAR(255) NOT NULL, \
+            SHOP_ID VARCHAR(255) NOT NULL, \
+            QUANTITY INTEGER NOT NULL, \
+            title TEXT, \
+            author TEXT, \
+            publisher TEXT, \
+            original_title TEXT, \
+            translator TEXT, \
+            pub_year TEXT, \
+            pages INTEGER, \
+            price INTEGER, \
+            currency_unit TEXT, \
+            binding TEXT, \
+            isbn TEXT, \
+            author_intro TEXT, \
+            book_intro TEXT, \
+            content TEXT, \
+            tags TEXT\
+            )\
+            ",
+            " CREATE TABLE ORDERS (\
+            ORDER_ID VARCHAR(255) PRIMARY KEY,\
+            UID VARCHAR(255) NOT NULL,\
+            SHOP_ID VARCHAR(255) NOT NULL, \
+            BOOK_ID VARCHAR(255) NOT NULL,\
+            ORDER_TIME TEXT NOT NULL, \
+            ORDER_QUANTITY INTEGER NOT NULL,\
+            CURRENT_STATE INTEGER NOT NULL\
+            )\
+            ",
+            "ALTER TABLE USERS \
+            ADD FOREIGN KEY (SHOP_ID) \
+            REFERENCES SHOPS(SHOP_ID) \
+            ",
+            "ALTER TABLE SHOPS \
+            ADD FOREIGN KEY (UID) \
+            REFERENCES USERS(UID) \
+            ",
+            "ALTER TABLE BOOKS\
+            ADD FOREIGN KEY (UID)\
+            REFERENCES USERS(UID),\
+            ADD FOREIGN KEY (SHOP_ID)\
+            REFERENCES SHOPS(SHOP_ID)\
+            ",
+            "ALTER TABLE ORDERS\
+            ADD FOREIGN KEY (UID)\
+            REFERENCES USERS(UID),\
+            ADD FOREIGN KEY (SHOP_ID)\
+            REFERENCES SHOPS(SHOP_ID),\
+            ADD FOREIGN KEY (BOOK_ID)\
+            REFERENCES BOOKS(BOOK_ID)\
+            "
+        )
+        try:
+            conn = self.db_connect()
+            cur = conn.cursor()
+            for command in commands:
+                cur.execute(command)
+            cur.close
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+            print('Database connection closed.')
 
     def config(self):
         parser = ConfigParser()
@@ -36,3 +121,6 @@ class DB_handler:
             print(error)
 
         return conn
+
+
+db_handler = DB_handler()
