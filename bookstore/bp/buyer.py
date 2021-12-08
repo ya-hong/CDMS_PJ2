e7003a5ec9b5df2ca8472ec99c18c9d65fe7f274
10195501422 <10195501422@stu.ecnu.edu.cn>
2021-12-08 14:43:05 +0800

    new

--- bookstore/bp/buyer.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-from flask import request
-
-bp = Blueprint('buyer', __name__, url_prefix = "/buyer")
-
--- bookstore/bp/seller.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-from flask import request
-
-bp = Blueprint('seller', __name__, url_prefix = "/seller")
-
