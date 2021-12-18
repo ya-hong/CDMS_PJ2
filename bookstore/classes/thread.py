49bfa7a31a268fd3661ccda5eac6d75862cf179e
10195501422 <10195501422@stu.ecnu.edu.cn>
2021-12-18 21:30:11 +0800

    thread updated

--- bookstore/classes/thread.py
+++ bookstore/classes/thread.py
@@ -1,66 +1,34 @@
 import threading
 import time
-from bookstore.classes.order import Order
 from bookstore.db_handler import DB_handler
 from bookstore.classes.sql import SQL
 from bookstore import error
 
 
-sleep_time = 10
+sleep_time = 5 * 60
 
 
 class orderThread(threading.Thread):
-    def __init__(self, option):
+    def __init__(self, order_id):
         threading.Thread.__init__(self)
-        self.latency = 3600
+        self.order_id = order_id
         self.conn = DB_handler().db_connect()
         self.sql = SQL()
-        self.option = option
 
     def time_check(self):
         threading.Lock.acquire()
-        ret = self.sql.transaction("SELECT order_id, order_time, shop_id FROM orders WHERE\
-                                    current_state != 0 order by order_time;", [])
-        if ret is None:
+        ret = self.sql.transaction("SELECT current_time, shop_id FROM orders WHERE order_id = %s;", [self.order_id])
+        if ret[0] != error.OrderState.UNPAID.value[0]:
             return
-        to_del_num = 0
-        for time_stamp in [float(i[1]) for i in ret]:
-            if (time.time() - time_stamp) >= self.latency:
-                to_del_num += 1
-            else:
-                break
-        to_del_info = [(i[0], i[2]) for i in ret][:to_del_num]
-        for order_id, shop_id in to_del_info:
-            self.sql.transaction("UPDATE orders SET current_state = %s WHERE order_id = %s",
-                                    [str(error.OrderState.AUTO_CANCEL.value[0]), order_id])
-            ret = self.sql.transaction("SELECT book_id, order_quantity FROM order_book WHERE order_id = %s", [order_id])
-            for (book_id, quantity) in ret:
-                self.sql.transaction("UPDATE book SET QUANTITY = QUANTITY + %s WHERE shop_id = %s AND book_id = %s",
-                                    [quantity, shop_id, book_id])
+        shop_id = ret[1]
+        self.sql.transaction("UPDATE orders SET current_state = %s WHERE order_id = %s",
+                                [str(error.OrderState.AUTO_CANCEL.value[0]), self.order_id])
+        ret = self.sql.transaction("SELECT book_id, order_quantity FROM order_book WHERE order_id = %s", [self.order_id])
+        for (book_id, quantity) in ret:
+            self.sql.transaction("UPDATE book SET QUANTITY = QUANTITY + %s WHERE shop_id = %s AND book_id = %s",
+                                [quantity, shop_id, book_id])
         threading.Lock.release()
 
-    # def deliver(self, order_id):
-    #     threading.Lock.acquire()
-    #     ret = self.sql.transaction("UPDATE orders SET current_state = %s WHERE order_id = %s",
-    #                                [order_id, str(error.OrderState.UNDELIVERED.value[0])])
-    #     if ret == 0:
-    #         raise
-    #     for order_id in [i[0] for i in ret]:
-    #         self.sql.transaction("UPDATE orders SET current_state = %s WHERE order_id = %s",
-    #                                 [str(error.OrderState.DELIVERED.value[0]), order_id])
-    #     threading.Lock.release()
-    #
     def run(self):
-        while True:
-            if self.option == 1:
-                self.time_check()
-            # else:
-            #     self.deliver()
-            time.sleep(sleep_time)
-
-
-if __name__ == "__main__":
-    thread_auto_cancel = orderThread(1)
-    # thread_deliver = orderThread(2)
-    thread_auto_cancel.start()
-    # thread_deliver.start()
+        time.sleep(sleep_time)
+        self.time_check()        
